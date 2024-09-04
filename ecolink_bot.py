bb256d1a12c2bc1554b375b2158d96bc8d81cf71
Sergerud <serge@rudnya.pp.ru>
2024-09-04 18:02:03 +0300

    Update ecolink_bot.py

--- ecolink_bot.py
+++ ecolink_bot.py
@@ -16,14 +16,25 @@ async def on_ready():
 
 @bot.command()
 async def tgarb(ctx):
-    img_name=random.choice(os.listdir("images"))
-    with open(f'images/{img_name}', 'rb') as f:
+    img_name=random.choice(os.listdir("garbage"))
+    with open(f'garbage/{img_name}', 'rb') as f:
         # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
         picture = discord.File(f)
         # Можем передавать файл как параметр!
+        print(f)
         await ctx.send(file=picture)   
-@bot.command('garbrec')
-async def garbrec(ctx):
-    fun=[get_fox_image_url(),get_duck_image_url(),get_dogs_image_url()]
-    image_url = random.choice(fun)
-    await ctx.send(image_url) 
+@bot.command('all')
+async def all(ctx):
+    #fun=['$metal,$steklo,$plastik']
+    #commands=(', '.join(fun))
+    await ctx.send(f"Привет! Я бот EcoLink. Я умею находить переработчика отходов из Стекла - команда $steklo, Пластика - команда $plastik, Металла - команда $metal")
+    #await ctx.send(commands) 
+@bot.command('metal')
+async def metal(ctx):
+      with open(f'garbage/metal.jpg', 'rb') as f:
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+        await ctx.send(file=picture)  
+        await ctx.send(f"Металл")
+
+bot.run('')    
